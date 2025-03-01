ff18333efe5778a42612cc6f2de77bfc57797e38
Dallin-Shmallin <dallin.mccaleb@gmail.com>
2025-03-01 13:20:47 -0700

    fix attempts

--- Scenes/stairs.gd
+++ Scenes/stairs.gd
@@ -14,3 +14,7 @@ func _process(delta: float) -> void:
 func activate() -> void:
 	isActive = true
 	visible = true
+
+
+func _on_area_entered(area: Area2D) -> void:
+	get_tree().change_scene_to_file("res://Scenes/Levels/level2.tscn")
--- Scripts/wall.gd
+++ Scripts/wall.gd
@@ -1,6 +1,7 @@
 extends Node2D
 @onready var collision = $StaticBody2D
 @onready var sprite = $Sprite2D
+var catcount = 0
 func disappear():
 	collision.process_mode = Node.PROCESS_MODE_DISABLED
 	sprite.modulate.a = .5
