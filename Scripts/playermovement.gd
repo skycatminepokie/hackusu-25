341392e0118a9b19921debcbde29570d7cbc73b3
Dallin-Shmallin <dallin.mccaleb@gmail.com>
2025-02-28 21:21:00 -0700

    broke movement for no diagnol

--- Scripts/playermovement.gd
+++ Scripts/playermovement.gd
@@ -1,21 +1,26 @@
 extends CharacterBody2D
-const SPEED = 300
+const SPEED = 500
 @onready var label = $Label
 func _physics_process(delta: float)-> void:
+	var inputdirection = Vector2(0,0)
 	var screen_position = global_position
-	var inputdirection_y := Input.get_axis("Up", "Down")
-	var inputdirection_x := Input.get_axis("Left", "Right")
-	if inputdirection_y > 0:
-		velocity.y = SPEED
-	if inputdirection_y < 0:
-		velocity.y = SPEED * -1
-	if inputdirection_y == 0:
-		velocity.y = 0
-	if inputdirection_x < 0:
-		velocity.x = SPEED * -1
-	if inputdirection_x > 0:
-		velocity.x = SPEED
-	if inputdirection_x == 0:
-		velocity.x = 0
-	label.text = str(screen_position)
+	inputdirection.y == Input.get_axis("Up", "Down")
+	inputdirection.x = Input.get_axis("Left", "Right")
+	match inputdirection:
+		Vector2(0,0):
+			velocity = Vector2.ZERO
+		Vector2(1,0):
+			velocity = Vector2(SPEED,0)
+		Vector2(-1,0):
+			velocity = Vector2(SPEED * -1,0)
+		Vector2(0,-1):
+			velocity = Vector2(0,SPEED)
+		Vector2(0,1):
+			velocity = Vector2(0,SPEED * 1)
+		
+	label.text = str(inputdirection)
+	if screen_position.y > 300 or screen_position.y < -300:
+		label.text = "I'm pushing the camera"
+	if screen_position.x > 600 or screen_position.x < -600:
+		label.text = "I'm pushing the camera"
 	move_and_slide()
