7482345e7daf705a5dd6b08ec44ab295b53ef5d4
Dallin-Shmallin <dallin.mccaleb@gmail.com>
2025-02-28 20:26:18 -0700

    minor fixes

--- Scripts/playermovement.gd
+++ Scripts/playermovement.gd
@@ -1,18 +1,21 @@
 extends CharacterBody2D
-const PLAYER_SPEED = 300
+const SPEED = 300
 func _physics_process(delta: float)-> void:
+	var screen_position = global_position
 	var inputdirection_y := Input.get_axis("Up", "Down")
 	var inputdirection_x := Input.get_axis("Left", "Right")
 	if inputdirection_y > 0:
-		velocity.y = PLAYER_SPEED
+		velocity.y = SPEED
 	if inputdirection_y < 0:
-		velocity.y = PLAYER_SPEED * -1
+		velocity.y = SPEED * -1
 	if inputdirection_y == 0:
 		velocity.y = 0
 	if inputdirection_x < 0:
-		velocity.x = PLAYER_SPEED * -1
+		velocity.x = SPEED * -1
 	if inputdirection_x > 0:
-		velocity.x = PLAYER_SPEED
+		velocity.x = SPEED
 	if inputdirection_x == 0:
 		velocity.x = 0
+	if screen_position.y > 600:
+		print("I'm pushing something")
 	move_and_slide()
