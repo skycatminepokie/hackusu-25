e722e5e1d745a9fab9ed7219ecec897000ecd432
Dallin-Shmallin <dallin.mccaleb@gmail.com>
2025-02-28 21:42:47 -0700

    fixed so that screen pos is accurate

--- Scripts/playermovement.gd
+++ Scripts/playermovement.gd
@@ -3,24 +3,24 @@ const SPEED = 500
 @onready var label = $Label
 func _physics_process(delta: float)-> void:
 	var inputdirection = Vector2(0,0)
-	var screen_position = global_position
+	var screen_position = get_global_transform_with_canvas().origin
 	inputdirection.y = Input.get_axis("Up", "Down")
 	inputdirection.x = Input.get_axis("Left", "Right")
 	match inputdirection:
 		Vector2(0,0):
 			velocity = Vector2.ZERO
 		Vector2(1,0):
-			velocity = Vector2(SPEED,0)
+			velocity = Vector2(SPEED, 0)
 		Vector2(-1,0):
-			velocity = Vector2(SPEED * -1,0)
+			velocity = Vector2(SPEED*-1, 0)
 		Vector2(0,-1):
-			velocity = Vector2(0,SPEED*-1)
+			velocity = Vector2(0, SPEED*-1)
 		Vector2(0,1):
-			velocity = Vector2(0,SPEED)
+			velocity = Vector2(0, SPEED)
 		
-	label.text = str(inputdirection)
-	if screen_position.y > 300 or screen_position.y < -300:
+	label.text = str(screen_position)
+	if screen_position.y > 840 or screen_position.y < 240:
 		label.text = "I'm pushing the camera"
-	if screen_position.x > 600 or screen_position.x < -600:
+	if screen_position.x > 1680 or screen_position.x < 240:
 		label.text = "I'm pushing the camera"
 	move_and_slide()
