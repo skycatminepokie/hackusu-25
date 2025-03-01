51c5222db6a92b545d610086a6ad2b19acc748d1
Dallin-Shmallin <dallin.mccaleb@gmail.com>
2025-02-28 22:12:35 -0700

    added collisions to camera and made pushback on edges

--- Scripts/playermovement.gd
+++ Scripts/playermovement.gd
@@ -1,7 +1,15 @@
 extends CharacterBody2D
-const SPEED = 500
+
 @onready var label = $Label
+func pushback(number,smallnumber,bignumber):
+	var newnumber = 0
+	if number > bignumber:
+		newnumber = (number - bignumber) * -1
+	if number < smallnumber:
+		newnumber = smallnumber - number / 14
+	return newnumber
 func _physics_process(delta: float)-> void:
+	var speed = 500
 	var inputdirection = Vector2(0,0)
 	var screen_position = get_global_transform_with_canvas().origin
 	inputdirection.y = Input.get_axis("Up", "Down")
@@ -10,17 +18,18 @@ func _physics_process(delta: float)-> void:
 		Vector2(0,0):
 			velocity = Vector2.ZERO
 		Vector2(1,0):
-			velocity = Vector2(SPEED, 0)
+			velocity = Vector2(speed, 0)
 		Vector2(-1,0):
-			velocity = Vector2(SPEED*-1, 0)
+			velocity = Vector2(speed*-1, 0)
 		Vector2(0,-1):
-			velocity = Vector2(0, SPEED*-1)
+			velocity = Vector2(0, speed*-1)
 		Vector2(0,1):
-			velocity = Vector2(0, SPEED)
+			velocity = Vector2(0, speed)
+	if screen_position.y > 840 or screen_position.y < 240:
+		velocity.y += pushback(screen_position.y, 240,840)
+	if screen_position.x > 1680 or screen_position.x < 240:
+		velocity.x += pushback(screen_position.x, 240, 1680)
 		
 	label.text = str(screen_position)
-	if screen_position.y > 840 or screen_position.y < 240:
-		label.text = "I'm pushing the camera"
-	if screen_position.x > 1680 or screen_position.x < 240:
-		label.text = "I'm pushing the camera"
+	
 	move_and_slide()
