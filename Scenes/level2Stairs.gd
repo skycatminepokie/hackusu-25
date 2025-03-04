5221c8e6227a9ad26c8e13bb1d80f5895c7d5c7d
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2025-03-04 15:12:54 -0700

    Make final level possible

--- Scenes/level2Stairs.gd
+++ Scenes/level2Stairs.gd
@@ -1,10 +1,16 @@
-extends "res://Scenes/stairs.gd"
+extends Node2D
+var isActive:bool = true
+
+func activate() -> void:
+	isActive = true
+	visible = true
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
+func _on_area_entered(area: Area2D) -> void:
+	get_tree().change_scene_to_file("res://Scenes/Levels/level3.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- Scenes/stairs.gd
+++ Scenes/stairs.gd
@@ -18,10 +18,3 @@ func activate() -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	get_tree().change_scene_to_file("res://Scenes/Levels/level2.tscn")
-	match get_tree().current_scene.name:
-		"test":
-			get_tree().change_scene_to_file("res://Scenes/Levels/level2.tscn")
-		"Level2":
-			get_tree().change_scene_to_file("res://Scenes/Levels/level3.tscn")
-		"level3":
-			get_tree().change_scene_to_file("res://Scenes/Levels/youwin.tscn")
